9522ba9b81ce5876715120bcc1aed7e6fac3ca73
DiyanLyubchev <Diyan.Lyubchev@gmail.com>
2022-11-06 12:12:47 +0200

    Add configurations for JWT into swagger

--- Simple.JWT.Token/Simple.JWT.Token/Extentions/ServiceCollectionExtensions.cs
+++ Simple.JWT.Token/Simple.JWT.Token/Extentions/ServiceCollectionExtensions.cs
@@ -5,7 +5,12 @@ using Microsoft.OpenApi.Models;
 using Sample.JWT.Token.Common;
 using Sample.JWT.Token.Interfaces;
 using Sample.JWT.Token.Managers;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System;
 using System.Text;
+using DocumentFormat.OpenXml.EMMA;
 
 namespace Simple.JWT.Token.Extentions
 {
@@ -44,9 +49,45 @@ namespace Simple.JWT.Token.Extentions
 
         public static IServiceCollection ResolveSwagger(this IServiceCollection services)
         {
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sample.JWT.Token", Version = "v1" });
+            //});
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sample.JWT.Token", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                              Type = ReferenceType.SecurityScheme,
+                              Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        
+                        },
+                        new List<string>()
+                    }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
 
             return services;
